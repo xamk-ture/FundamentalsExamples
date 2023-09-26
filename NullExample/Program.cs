c1ed9dd783b26fc912db00d418241889b5905a32
Tuomas Reijonen <hture01@xamk.fi>
2023-09-26 10:47:14 +0300

    Added more examples

---
+++ NullExample/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace NullExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = null;
+
+            try
+            {
+                // Yritetään kutsua metodia null-arvolla, mikä aiheuttaa NullReferenceExceptionin
+                int length = text.Length;
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine($"Poikkeus: {ex.Message}");
+            }
+        }
+    }
+}
