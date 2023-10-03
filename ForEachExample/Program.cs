de09fb923079c1c404a285d72a2217886001ea70
Tuomas Reijonen <hture01@xamk.fi>
2023-10-03 10:58:31 +0300

    Added for each example

---
+++ ForEachExample/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ForEachExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Alustetaan string-tyyppinen taulukko, joka sisältää eri eläimiä.
+            string[] elaimet = { "Koira", "Kissa", "Hevonen", "Kani" };
+
+            // 'foreach' -silmukkaa käytetään käymään läpi kaikki taulukon (tai muiden kokoelmatyyppien) elementit yksi kerrallaan.
+            // Tässä esimerkissä 'elain' on väliaikainen muuttuja, joka saa arvokseen kulloisenkin taulukon elementin.
+            foreach (string elain in elaimet)
+            {
+                // Tulostetaan kulloisenkin taulukon elementin arvo.
+                Console.WriteLine(elain);
+            }
+
+            // Ohjelman suorituksen lopussa näet tulostuksen:
+            // Koira
+            // Kissa
+            // Hevonen
+            // Kani
+        }
+    }
+}
