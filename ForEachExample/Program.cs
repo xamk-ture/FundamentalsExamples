87debdac0a4f42914f11f3a361f85976d906efa7
Tuomas Reijonen <hture01@xamk.fi>
2023-10-03 10:59:56 +0300

    More examples

--- ForEachExample/Program.cs
+++ ForEachExample/Program.cs
@@ -20,6 +20,18 @@
             // Kissa
             // Hevonen
             // Kani
+
+
+            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5 };
+
+
+            //foreachin sisälle voidaan myös tehdä logiikkaa. Tässä tapauksessa tulostetaan vain rivit, joidenka arvo
+            //on suurempi kuin 2
+            foreach (var number in numbers)
+            {
+                if(number > 2)
+                    Console.WriteLine(number);
+            }
         }
     }
 }
