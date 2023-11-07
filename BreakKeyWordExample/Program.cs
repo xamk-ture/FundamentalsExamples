bd9425790923117473ddeba5249ed15eb8886f4d
Tuomas Reijonen <hture01@xamk.fi>
2023-11-07 10:34:58 +0200

    Added break and continue key word examples

---
+++ BreakKeyWordExample/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace BreakKeyWordExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ForBreakExample();
+            SwitchBreakExample();
+        }
+
+        static void ForBreakExample()
+        {
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i == 5)
+                {
+                    break; // ends the loop when i is 5
+                }
+                Console.WriteLine(i); // prints numbers 1-4
+            }
+        }
+
+        static void SwitchBreakExample()
+        {
+            int numero = 3;
+
+            switch (numero)
+            {
+                case 1:
+                    Console.WriteLine("Number is one");
+                    break;
+                case 2:
+                    Console.WriteLine("Number is three");
+                    break;
+                case 3:
+                    Console.WriteLine("Number is three");
+                    break;
+                default:
+                    Console.WriteLine("Number is not one, two or three");
+                    break;
+            }
+
+        }
+    }
+}
---
+++ ContinueKeyWordExamples/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ContinueKeyWordExamples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WhileContinue();
+            ForEachContinue();
+        }
+
+        static void WhileContinue()
+        {
+            Console.WriteLine("While continue example");
+            int i = 0;
+            while (i < 10)
+            {
+                i++;
+
+                //Skip even numbers
+                if (i % 2 == 0)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+        }
+
+        static void ForEachContinue()
+        {
+            Console.WriteLine("ForEach continue example");
+
+            int[] numbers = { 1, 2, 3, 4, 5 };
+
+            foreach (var number in numbers)
+            {
+                //Skip even numbers
+                if(number % 2 == 0)
+                {
+                    continue;
+                }
+
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
