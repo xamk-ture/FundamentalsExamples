48944f7464a1cb20b8010856528ad96db9033e7c
Tuomas Reijonen <hture01@xamk.fi>
2024-09-17 12:53:50 +0300

    Added if example

---
+++ IfExample/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace IfExample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("If example that compares numbers");
+
+            Console.WriteLine("Give number 1");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Give number 2");
+            int num2 = int.Parse(Console.ReadLine());
+
+            if(num1 > num2) 
+            {
+                Console.WriteLine("The first number inputted is bigger");
+            }
+            else if(num1 < num2) 
+            {
+                Console.WriteLine("The second number inputted is bigger");
+            }
+            else if(num1 == num2)
+            {
+                Console.WriteLine("The number is same");
+            }
+
+
+        }
+    }
+}
